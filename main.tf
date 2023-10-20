8561cd8a3eeee0fdf3e27b828aedc28302b9a617
Samba73 <sambasivam.k@gmail.com>
2023-10-20 09:48:29 +0000

    made changes to main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,21 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+variable "instance_type" {
+  type = string
+}
+locals{
+  name = "sk"
+}
 resource "aws_instance" "my-server" {
   ami           = "ami-0556fb70e2e8f34b7"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
-    Name    = "my-server"
+    Name    = "my-server-${local.name}"
   }
 }
+/*
 resource "aws_instance" "my-server-2" {
   ami           = "ami-0556fb70e2e8f34b7"
   instance_type = "t2.micro"
@@ -26,4 +33,9 @@ resource "aws_instance" "my-server-2" {
   tags = {
     Name    = "my-server-2"
   }
+}
+*/
+
+output "instance_ip_addr" {
+  value = aws_instance.my-server.public_ip
 }

7fd616a2fdf4f1e4d99ae434073ceb1eb5d1db96
Samba73 <sambasivam.k@gmail.com>
2023-11-09 22:46:36 +0000

    wip

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "my-server" {
-  ami           = "ami-0556fb70e2e8f34b7"
-  instance_type = var.instance_type
+# resource "aws_instance" "my-server" {
+#   ami           = "ami-0556fb70e2e8f34b7"
+#   instance_type = var.instance_type
 
-  tags = {
-    Name    = "my-server-${local.name}"
-  }
-}
+#   tags = {
+#     Name    = "my-server-${local.name}"
+#   }
+# }
 /*
 resource "aws_instance" "my-server-2" {
   ami           = "ami-0556fb70e2e8f34b7"
@@ -17,7 +17,11 @@ resource "aws_instance" "my-server-2" {
 }
 */
 
-module "global" {
-  source = "./modules/eu"
+# module "global" {
+#   source = "./modules/eu"
     
+# }
+module "iam_iam-github-oidc-provider" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-github-oidc-provider"
+  version = "5.30.1"
 }

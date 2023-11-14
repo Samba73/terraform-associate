f0658ad7ba5d68903ff4e27a54a3029db0d42b3f
Samba73 <sambasivam.k@gmail.com>
2023-11-14 23:06:24 +0000

    secrets wip

--- main.tf
+++ main.tf
@@ -1,11 +1,39 @@
-# resource "aws_instance" "my-server" {
-#   ami           = "ami-0556fb70e2e8f34b7"
-#   instance_type = var.instance_type
+data "aws_iam_policy_document" "assume-role"{
+  statement {
+    actions = ["sts:AssumeRole"]
 
-#   tags = {
-#     Name    = "my-server-${local.name}"
-#   }
-# }
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role" "instance" {
+  name = "instance_role"
+  assume_role_policy = data.aws_iam_policy_document.assume-role.json
+}
+data "aws_iam_policy_document" "permission_policy"{
+  statement {
+    effect = "Allow"
+    actions = ["ec2:*"]
+    resources = ["*"]
+  }
+}
+resource "aws_iam_role_policy" "instance_role" {
+  role = aws_iam_role.instance.id
+  policy = data.aws_iam_policy_document.permission_policy.json
+}
+resource "aws_iam_instance_profile" "instance_profile" {
+  role = aws_iam_role.instance.name
+}
+resource "aws_instance" "my-server" {
+  ami           = "ami-0556fb70e2e8f34b7"
+  instance_type = var.instance_type
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
+  tags = {
+    Name    = "my-server-${local.name}"
+  }
+}
 /*
 resource "aws_instance" "my-server-2" {
   ami           = "ami-0556fb70e2e8f34b7"

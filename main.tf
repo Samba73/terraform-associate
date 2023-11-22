0226e12b1a649a08b0f57cba855edd6b0f949791
Samba73 <sambasivam.k@gmail.com>
2023-11-22 20:55:31 +0000

    WIP

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 data "aws_iam_policy_document" "assume-role"{
   statement {
     actions = ["sts:AssumeRole"]
@@ -34,6 +35,7 @@ resource "aws_instance" "my-server" {
     Name    = "my-server-${local.name}"
   }
 }
+*/
 /*
 resource "aws_instance" "my-server-2" {
   ami           = "ami-0556fb70e2e8f34b7"
@@ -63,3 +65,67 @@ resource "aws_instance" "my-server-2" {
 #   #   Name = each.key
 #   # }
 # }
+data "aws_caller_identity" "self" {}
+/*
+data "aws_iam_policy_document" "cmk_admin_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["kms:*"]
+    resources = ["*"]
+    principals {
+      type = "AWS"
+      identifiers = [data.aws_caller_identity.self.arn]
+    }
+  }
+} 
+
+resource "aws_kms_key" "cmk" {
+  policy = data.aws_iam_policy_document.cmk_admin_policy.json
+}
+
+resource "aws_kms_alias" "cmk" {
+  name = "alias/kms-cmk-2"
+  target_key_id = aws_kms_key.cmk.id
+}
+
+
+data "aws_kms_secrets" "creds" {
+  secret {
+    name = "test"
+    payload = file("${path.module}/creds.yml.encrypted")
+    key_id = aws_kms_alias.cmk.target_key_id
+  }
+  
+}
+locals {
+  creds = yamldecode(data.aws_kms_secrets.creds.plaintext["test"])
+}
+
+output "username" {
+  value = local.creds.username
+  sensitive = true
+}
+output "password" {
+  value = local.creds.password
+  sensitive = true
+}
+*/
+data "aws_secretsmanager_secret" "creds" {
+  name = "creds"
+}
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = data.aws_secretsmanager_secret.creds.id
+}
+locals {
+  creds = jsondecode(data.aws_secretsmanager_secret_version.creds.secret_string)
+}
+
+output "username" {
+  value = local.creds.username
+  sensitive = true
+}
+output "password" {
+  value = local.creds.password
+  sensitive = true
+  
+}
